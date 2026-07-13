bffc0eb978da97b8356b0243d4666629e3016145
Balázs Püspök-Kiss <pkblazsak@gmail.com>
2026-07-13 16:25:48 +0200

    Enable MDX content rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
@@ -8,7 +9,7 @@ export default defineConfig({
   site,
   output: "static",
   trailingSlash: "always",
-  integrations: [icon(), sitemap()],
+  integrations: [mdx(), icon(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "github-dark-dimmed",
